562ce019d477e6b61cdd76076e62cb661d9a06a5
irene-myan <i2martir@uwaterloo.ca>
2022-01-23 00:38:25 -0500

    Dungeon scene w/ portal

---
+++ Farmer/Assets/Chest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chest : Collectable
+{
+    public Sprite noCoins;
+    public int numCoins = 5;
+
+    protected override void OnCollect()
+    {
+        if (!collected)
+        {
+            base.OnCollect();
+            Debug.Log("COIN " + numCoins);
+            GetComponent<SpriteRenderer>().sprite = noCoins;
+        }
+    }
+}
